a2ed0bbaad9ffcf24097afadcf58ccdd6688b990
yinqiao <1737666820@qq.com>
2023-11-08 16:10:17 +0800

    feat: 优化文档

--- views-doc/src/layout/components/listen-schema-update/index.tsx
+++ views-doc/src/layout/components/listen-schema-update/index.tsx
@@ -3,6 +3,7 @@ import { onSchemaDiffToOperationDefs } from '@fruits-chain/qiufen-pro-helpers'
 import { Tooltip } from 'antd'
 import { buildSchema } from 'graphql'
 import React, { useState, useMemo, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import useBearStore from '@/stores'
 
@@ -49,10 +50,16 @@ const ListenSchemaUpdate: FC<IProps> = () => {
     }
   }, [hasUpdateInfo])
 
+  const navigate = useNavigate()
+
   return (
-    <div style={{ marginLeft: 12 }}>
+    <div
+      style={{ marginLeft: 12 }}
+      onClick={() => {
+        navigate('/home')
+      }}>
       {hasUpdateInfo ? (
-        <Tooltip title={<span>有更新</span>}>
+        <Tooltip title={<span>有更新（点击跳转到差异对比页面进行查看）</span>}>
           <BellTwoTone twoToneColor="#52c41a" style={{ fontSize: 24 }} />
         </Tooltip>
       ) : null}
